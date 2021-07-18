3d2003502da230fe7559dce9b461c06da2c399ac
rezasmoradi <reza.seidmoradi@outlook.com>
2021-07-18 14:30:44 +0430

    added request method to routing

--- core/Router.php
+++ core/Router.php
@@ -10,6 +10,8 @@ class Router
 
     protected $params = [];
 
+    protected $namespace = 'App\Controllers\\';
+
     public function add($route, $params = [])
     {
         $route = preg_replace('/^\//', '', $route);
@@ -21,21 +23,51 @@ class Router
         $this->routes[$route] = $params;
     }
 
+    public function __call($name, $args)
+    {
+        if (gettype($args[1]) === 'object') {
+            self::add($args[0], $args[1]);
+        } else {
+            $args[1]['method'] = strtoupper($name);
+            self::add($args[0], $args[1]);
+        }
+    }
+
     public function match($url)
     {
         foreach ($this->routes as $route => $params) {
             if (preg_match($route, $url, $matches)) {
-                foreach ($matches as $key => $match) {
-                    if (is_string($key)) {
-                        $params[$key] = $match;
-                    }
+                if ($method = $this->checkMethod(array_keys($params))) {
+                    $this->setParams($matches, $params[$method]);
+                    return true;
                 }
-
-                $this->params = $params;
-                return true;
             }
         }
         return false;
+    }
+
+    private function checkMethod($methods)
+    {
+        if (count($methods) > 1) {
+            foreach ($methods as $method) {
+                if ($method === $_SERVER['REQUEST_METHOD']) {
+                    return $method;
+                }
+            }
+        } else {
+            return $methods[0] === $_SERVER['REQUEST_METHOD'] ? $methods[0] : false;
+        }
+        return false;
+    }
+
+    private function setParams($matches, $params)
+    {
+        foreach ($matches as $key => $match) {
+            if (is_string($key)) {
+                $params['params'][$key] = $match;
+            }
+        }
+        $this->params = $params;
     }
 
     public function dispatch($url)
@@ -44,6 +76,7 @@ class Router
 
         if ($this->match($url)) {
             $controller = $this->params['controller'];
+            $controller = $this->getNamespace() . $controller;
 
             if (class_exists($controller)) {
                 $obj = new $controller($this->params);
@@ -70,6 +103,15 @@ class Router
         }
 
         return $url;
+    }
+
+    public function getNamespace()
+    {
+        if (array_key_exists('namespace', $this->params)) {
+            $this->namespace .= $this->params['namespace'];
+        }
+
+        return $this->namespace;
     }
 
     /**
--- public/index.php
+++ public/index.php
@@ -7,6 +7,9 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 $router = new Router();
 
 $router->add('/', ['controller' => 'HomeController', 'action' => 'index']);
+$router->get('/login', ['controller' => 'HomeController', 'action' => 'index']);
+$router->post('/login', ['controller' => 'HomeController', 'action' => 'index']);
+$router->post('/logout', ['controller' => 'HomeController', 'action' => 'index']);
 
 try {
     $router->dispatch($_SERVER['QUERY_STRING']);

27e25adccce18922670c7185c6b9bb29889b03a6
rezasmoradi <reza.seidmoradi@outlook.com>
2021-07-18 14:57:13 +0430

    added error handler class

---
+++ core/Error.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace Core;
+
+
+use App\Config;
+
+class Error
+{
+    public static function errorHandler($level, $message, $file, $line)
+    {
+        if (error_reporting() !== 0) {
+            throw new \ErrorException($message, 0, $level, $file, $line);
+        }
+    }
+
+    /**
+     * @param \Exception $exception
+     */
+    public static function exceptionHandler($exception)
+    {
+        $code = $exception->getCode();
+        if ($code !== 404) {
+            $code = 500;
+        }
+        http_response_code($code);
+
+        if (true) {
+            echo "<h1>Fatal error</h1>";
+            echo "<p>Uncaught exception: '" . get_class($exception) . "'</p>";
+            echo "<p>Message: '" . $exception->getMessage() . "'</p>";
+            echo "<p>Stack trace:<pre>" . $exception->getTraceAsString() . "</pre></p>";
+            echo "<p>Thrown in '" . $exception->getFile() . "' on line " . $exception->getLine() . "</p>";
+
+            View::renderTemplate("errors/$code");
+
+        } else {
+            $log = dirname(__DIR__) . 'storage/logs/' . date('Y-m-d') . '.log';
+            ini_set('error_log', $log);
+
+            $message = "Uncaught exception: '" . get_class($exception) . "'";
+            $message .= " with message '" . $exception->getMessage() . "'";
+            $message .= "\nStack trace: " . $exception->getTraceAsString();
+            $message .= "\nThrown in '" . $exception->getFile() . "' on line " . $exception->getLine();
+
+            error_log($message);
+        }
+    }
+
+    public function fatalShutdown()
+    {
+        var_dump(error_get_last());
+    }
+}
--- public/index.php
+++ public/index.php
@@ -4,6 +4,11 @@ use Core\Router;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
+error_reporting(E_ALL);
+set_error_handler('Core\Error::errorHandler');
+set_exception_handler('\Core\Error::exceptionHandler');
+register_shutdown_function('\Core\Error::fatalShutdown');
+
 $router = new Router();
 
 $router->add('/', ['controller' => 'HomeController', 'action' => 'index', 'method' => 'get']);
